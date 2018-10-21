2952ca95aaba5d6cb85bdfd1d8c88387c6191472
choinamki <uyom589@gmail.com>
2018-10-21 23:20:40 +0900

    pipe3.c

---
+++ pipe3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+
+int main(void)
+{
+	int pfd[2];
+
+	if(pipe(pfd) == -1)
+	{
+		perror("pipe error");
+		exit(1);
+	}
+
+	if(!fork())
+	{
+		close(1);
+		dup(pfd[1]);
+		close(pfd[0]);
+
+		execlp("is", "is", NULL);
+	}
+	else
+	{
+		close(0);
+		dup(pfd[0]);
+		close(pfd[1]);
+
+		execlp("wc","wc", "-1",NULL);
+	}
+	
+	return 0;
+}
