c098eac45d12ac0fb48f78f758465e2f058ea999
choinamki <41056045+choinamki@users.noreply.github.com>
2018-10-22 01:07:28 +0900

    Update sigactionex1.c
    remove korean

--- sigactionex1.c
+++ sigactionex1.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
-#include <signal.h>
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
 
 struct sigaction act_new;
 struct sigaction act_old;
 
 void sigint_handler( int signo)
 {
-   printf( "Ctrl-C 키를 눌루셨죠!!\n");
-   printf( "또 누르시면 종료됩니다.\n");
+   printf( "ctrl + c");
+   printf( "again exit.\n");
    sigaction( SIGINT, &act_old, NULL);
 }
 
 int main( void)
 {
    
-   act_new.sa_handler = sigint_handler; // 시그널 핸들러 지정
-   sigemptyset( &act_new.sa_mask);      // 시그널 처리 중 블록될 시그널은 없음
-                                           
-   // SIGINT를 지정하면서 act_old에 이전 정보를 구합니다.                                           
+   act_new.sa_handler = sigint_handler; 
+   sigemptyset( &act_new.sa_mask);      
+                                                                                      
    sigaction( SIGINT, &act_new, &act_old); 
    while(1 )
    {
